d51192db6f504989734b7294de421475069a35b2
himanshu-metaversity <himanshu@metaversitytechnologies.com>
2024-06-06 18:26:21 +0530

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,27 @@ import "./App.css"
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Mainlayout from "./Common/Mainlayout/Mainlayout"
 import Dashboard from "./Pages/Dashboard/Dashboard"
+import Login from "./Pages/login/Login"
 
 const App = () => {
   return (
     <div className="App">
       <Router>
-          <>
-            <Routes>
-              {/* <Route path="/login" element={<Login />} /> */}
-              {/* <Route path="/changePassword" element={<ChangePassword />} /> */}
-              <Route
-                path="/"
-                element={
-                  <Mainlayout/>
-                }>
-                  <Route path="/" element={<Dashboard />} />
-                
-              </Route>
-            </Routes>
-          </>
-        </Router>
+        <>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/changePassword" element={<ChangePassword />} /> */}
+            <Route
+              path="/"
+              element={
+                <Mainlayout />
+              }>
+              <Route path="/" element={<Dashboard />} />
+
+            </Route>
+          </Routes>
+        </>
+      </Router>
     </div>
   )
 }

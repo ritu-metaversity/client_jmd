d998b895c9012c8c4cf6c2e2a6418db2fc12cc1b
ritu-metaversity <rituraj@metaversitytechnologies.com>
2024-06-07 11:55:30 +0530

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,30 @@ import Inplay from "./Pages/Inplay/Inplay"
 import LiveGames from "./Pages/LiveGames/LiveGames"
 import OddsData from "./Pages/OddsData/OddsData"
 import CompleteGames from "./Pages/CompleteGames/CompleteGames"
+import Rules from "./Pages/Rules/Rules"
+import TermsConditions from "./Pages/TermsConditions/TermsConditions"
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
       <>
             <Routes>
               <Route path="/login" element={<Login />} />
+              <Route path="/main" element={<TermsConditions />} />
               <Route
                 path="/"
                 element={
                   <Mainlayout/>
                 }>
                   <Route path="/" element={<Dashboard />} />
+                  
                   <Route path="/main/upcoming-games" element={<UpcomingGames />} />
                   <Route path="/main/inplay" element={<Inplay />} />
                   <Route path="/main/live-games" element={<LiveGames />} />
                   <Route path="/main/bet-details/:id" element={<OddsData />} />
                   <Route path="/main/complete-games" element={<CompleteGames />} />
+                  <Route path="/main/rules" element={<Rules />} />
                 
               </Route>
             </Routes>
--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -1,81 +1,106 @@
-import { Box, Button, FilledInput, FormControl, TextField, Typography } from '@mui/material'
-import './styles.scss';
-import bgImage from '../../assets/img/bg-01.webp';
-import jmdlogo from '../../assets/img/jmd-white-logo.webp';
+import {
+  Box,
+  Button,
+  FilledInput,
+  FormControl,
+  Typography,
+} from "@mui/material"
+import "./styles.scss"
+import bgImage from "../../assets/img/bg-01.webp"
+import jmdlogo from "../../assets/img/jmd-white-logo.webp"
+import { useNavigate } from "react-router"
 
 const Login = () => {
-    const handlerSubmit = (e: any) => {
-        e.preventDefault();
+  const nav = useNavigate()
+  const handlerSubmit = (e: any) => {
+    e.preventDefault()
+    nav("/main")
+  }
 
-    }
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+      className="loginparent"
+    >
+      <Box sx={{}} className="loginformparent">
+        <Box className="wrapper">
+          <img src={jmdlogo} className="jmdloginlogo" alt="" />
+          <Typography component="p" className="msg"></Typography>
+        </Box>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <form onSubmit={handlerSubmit} className="form_">
+            <FormControl variant="filled" sx={{ width: "100%" }}>
+              <FilledInput
+                sx={{
+                  padding: "4px 0",
+                  width: "100%",
+                  borderRadius: "0",
+                  "::placeholder": { color: "red" },
+                }}
+                id="component-filled"
+                placeholder="User name"
+              />
+            </FormControl>
+            <FormControl variant="filled" sx={{ width: "100%" }}>
+              <FilledInput
+                sx={{
+                  padding: "4px 0",
+                  width: "100%",
+                  borderRadius: "0",
+                  "&::placeholder": { backgroundColor: "red" },
+                }}
+                id="component-filled"
+                placeholder="Password"
+              />
+            </FormControl>
 
+            <Button
+            type="submit"
+              sx={{
+                ":hover": {
+                  backgroundColor: "#D41872",
+                },
 
-    return (
-        <Box sx={{
-            backgroundImage: `url(${bgImage})`,
-        }} className="loginparent">
-            <Box sx={{}} className="loginformparent">
-                <Box className="wrapper">
-                    <img src={jmdlogo} className='jmdloginlogo' alt="" />
-                    <Typography component="p" className='msg'></Typography>
-                </Box>
-                <Box
-                    sx={{
-                        width: 500,
-                        maxWidth: '100%',
-                    }}
-                >
-                    <form onSubmit={handlerSubmit} className='form_'>
+                mt: 3,
+                textTransform: "uppercase",
+                fontFamily: "ubuntu-bold",
+                backgroundColor: "#D41872",
+                padding: "0 20px",
+                minWidth: "160px",
+                height: "42px",
+                borderRadius: "21px",
+                fontSize: "18px",
+                color: "#fff",
+              }}
+            >
+              submit
+            </Button>
+          </form>
 
-                        <FormControl variant="filled" sx={{ width: '100%' }}>
-                            <FilledInput sx={{
-                                padding: '4px 0',
-                                width: '100%',
-                                borderRadius: '0',
-                                '::placeholder': { color: 'red' }
-                            }} id="component-filled" placeholder="User name" />
-                        </FormControl>
-                        <FormControl variant="filled" sx={{ width: '100%' }}>
-                            <FilledInput
-                                sx={{
-                                    padding: '4px 0',
-                                    width: '100%',
-                                    borderRadius: '0',
-                                    '&::placeholder': { backgroundColor: 'red' }
-                                }} id="component-filled" placeholder="Password" />
-                        </FormControl>
-
-                        <Button sx={{
-                            ":hover": {
-                                backgroundColor: '#D41872'
-                            },
-
-                            mt: 3,
-                            textTransform: 'uppercase',
-                            fontFamily: 'ubuntu-bold',
-                            backgroundColor: '#D41872',
-                            padding: '0 20px',
-                            minWidth: '160px',
-                            height: '42px',
-                            borderRadius: '21px',
-                            fontSize: '18px',
-                            color: '#fff'
-                        }}>
-                            submit
-                        </Button>
-
-                    </form>
-
-                    <Typography component='h6' sx={{ mt: 4, fontFamily: 'Ubuntu-Bold', color: '#fff', fontSize: '16px' }}>
-                        ALL ABOUT FOR ONLINE GAME ZONE © 2021
-                        <br />
-                        JMD SPORTS11.COM
-                    </Typography>
-                </Box>
-
-            </Box>
-        </Box >
-    )
+          <Typography
+            component="h6"
+            sx={{
+              mt: 4,
+              fontFamily: "Ubuntu-Bold",
+              color: "#fff",
+              fontSize: "16px",
+            }}
+          >
+            ALL ABOUT FOR ONLINE GAME ZONE © 2021
+            <br />
+            JMD SPORTS11.COM
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default Login

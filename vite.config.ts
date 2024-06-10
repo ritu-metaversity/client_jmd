c6aa3073d20581a449429ff423e1ccd251791ef2
ritu-metaversity <rituraj@metaversitytechnologies.com>
2024-06-10 16:26:44 +0530

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vitest/config"
+// import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     open: true,
+//   },
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "src/setupTests",
+//     mockReset: true,
+//   },
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    host:true
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  }
 })
